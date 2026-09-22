d9184aaf23830222d7d73bae0ffde6795db2e5d0
Irwan0926 <muhammadirwantaufik26@gmail.com>
2026-09-22 10:44:42 +0700

    perubahan pada routes/auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 // Url untuk guest
+// perubahana n route untuk login, register, forgot password, reset password
 Route::middleware('guest')->group(function () {
     
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
